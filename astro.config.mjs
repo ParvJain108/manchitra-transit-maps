819c7dd026207fc5ad25f5c0a7f7d2e4294e2a26
ParvJain108 <parvj23@iitk.ac.in>
2026-07-18 19:12:14 +0530

    Sitemap Addition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://github.com/ParvJain108',
+  site: 'https://parvjain108.github.io',
   base: '/manchitra-transit-maps/',
+  integrations: [sitemap()],
 });
